15e507a84ed7c07d3ff6a9bc509b51e9dcabafd5
James Hanlon <mail@jameswhanlon.com>
2025-04-21 11:26:04 +0100

    Output dotfile

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "slang/analysis/AbstractFlowAnalysis.h"
 #include "slang/util/IntervalMap.h"
 #include "slang/util/BumpAllocator.h"
+#include "slang/text/FormatBuffer.h"
 
 #include "DirectedGraph.hpp"
 
@@ -330,6 +331,57 @@ public:
     }
 };
 
+void printDOT(const NetlistGraph& netlist, const std::string& fileName) {
+    slang::FormatBuffer buffer;
+    buffer.append("digraph {\n");
+    buffer.append("  node [shape=record];\n");
+//    for (auto& node : netlist) {
+//        switch (node->kind) {
+//            case NodeKind::PortDeclaration: {
+//                auto& portDecl = node->as<NetlistPortDeclaration>();
+//                buffer.format("  N{} [label=\"Port declaration\\n{}\"]\n", node->ID,
+//                              portDecl.hierarchicalPath);
+//                break;
+//            }
+//            case NodeKind::VariableDeclaration: {
+//                auto& varDecl = node->as<NetlistVariableDeclaration>();
+//                buffer.format("  N{} [label=\"Variable declaration\\n{}\"]\n", node->ID,
+//                              varDecl.hierarchicalPath);
+//                break;
+//            }
+//            case NodeKind::VariableAlias: {
+//                auto& varAlias = node->as<NetlistVariableAlias>();
+//                buffer.format("  N{} [label=\"Variable alias\\n{}\"]\n", node->ID,
+//                              varAlias.hierarchicalPath);
+//                break;
+//            }
+//            case NodeKind::VariableReference: {
+//                auto& varRef = node->as<NetlistVariableReference>();
+//                if (!varRef.isLeftOperand())
+//                    buffer.format("  N{} [label=\"{}\\n\"]\n", node->ID, varRef.toString());
+//                else if (node->edgeKind == EdgeKind::None)
+//                    buffer.format("  N{} [label=\"{}\\n[Assigned to]\"]\n", node->ID,
+//                                  varRef.toString());
+//                else
+//                    buffer.format("  N{} [label=\"{}\\n[Assigned to @({})]\"]\n", node->ID,
+//                                  varRef.toString(), toString(node->edgeKind));
+//                break;
+//            }
+//            default:
+//                SLANG_UNREACHABLE;
+//        }
+//    }
+//    for (auto& node : netlist) {
+//        for (auto& edge : node->getEdges()) {
+//            if (!edge->disabled) {
+//                buffer.format("  N{} -> N{}\n", node->ID, edge->getTargetNode().ID);
+//            }
+//        }
+//    }
+    buffer.append("}\n");
+    OS::writeFile(fileName, buffer.str());
+}
+
 
 int main(int argc, char** argv) {
     Driver driver;
@@ -337,8 +389,13 @@ int main(int argc, char** argv) {
 
     std::optional<bool> showHelp;
     std::optional<bool> showVersion;
+    std::optional<std::string> netlistDotFile;
     driver.cmdLine.add("-h,--help", showHelp, "Display available options");
     driver.cmdLine.add("--version", showVersion, "Display version information and exit");
+    
+    driver.cmdLine.add("--netlist-dot", netlistDotFile,
+                       "Dump the netlist in DOT format to the specified file, or '-' for stdout",
+                       "<file>", CommandLineFlags::FilePath);
 
     if (!driver.parseCommandLine(argc, argv))
         return 1;
@@ -357,7 +414,7 @@ int main(int argc, char** argv) {
 
     if (!driver.processOptions())
         return 2;
-
+    
     bool ok = driver.parseAllSources();
     auto compilation = driver.createCompilation();
     driver.reportCompilation(*compilation, true);
@@ -368,6 +425,12 @@ int main(int argc, char** argv) {
 
     NetlistVisitor visitor(*compilation, graph);
     compilation->getRoot().visit(visitor);
+        
+    // Output a DOT file of the netlist.
+    if (netlistDotFile) {
+        printDOT(graph, *netlistDotFile);
+        return 0;
+    }
 
     return ok ? 0 : 3;
 }
