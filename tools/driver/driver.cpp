42663b937c2f265c6405aca1083f6e09a81859a7
James Hanlon <mail@jameswhanlon.com>
2025-06-27 21:47:33 +0100

    Import some driver code

--- tools/driver/driver.cpp
+++ tools/driver/driver.cpp
@@ -1,5 +1,5 @@
-#include "slang/ast/Compilation.h"
 #include "slang/driver/Driver.h"
+#include "slang/ast/Compilation.h"
 #include "slang/text/FormatBuffer.h"
 #include "slang/util/VersionInfo.h"
 
@@ -34,36 +34,39 @@ void printDOT(const NetlistGraph &netlist, const std::string &fileName) {
     switch (node->kind) {
     case NodeKind::PortDeclaration: {
       auto &portDecl = node->as<PortDeclaration>();
-      //buffer.format("  N{} [label=\"Port declaration\\n{}\"]\n", node->ID,
-      //              portDecl.hierarchicalPath);
+      // buffer.format("  N{} [label=\"Port declaration\\n{}\"]\n", node->ID,
+      //               portDecl.hierarchicalPath);
       break;
     }
     case NodeKind::VariableDeclaration: {
       auto &varDecl = node->as<VariableDeclaration>();
-      //buffer.format(
-      //    "  N{} [label=\"Variable declaration\\n {}\"]\n",
-      //    node->ID, varDecl.hierarchicalPath);
+      // buffer.format(
+      //     "  N{} [label=\"Variable declaration\\n {}\"]\n",
+      //     node->ID, varDecl.hierarchicalPath);
       break;
     }
     case NodeKind::VariableAlias: {
       auto &varAlias = node->as<VariableAlias>();
-      //buffer.format("  N{} [label=\"Variable alias\\n{}\"]\n", node->ID,
-      //              varAlias.hierarchicalPath);
+      // buffer.format("  N{} [label=\"Variable alias\\n{}\"]\n", node->ID,
+      //               varAlias.hierarchicalPath);
       break;
     }
     case NodeKind::VariableReference: {
       auto &varRef = node->as<VariableReference>();
-      //if (!varRef.isLeftOperand()) {
-      //  buffer.format("  N{} [label=\"{}\\n\"]\n", node->ID, varRef.toString());
-      //} else if (node->edgeKind == EdgeKind::None) {
-      //  buffer.format("  N{} [label=\"{}\\n[Assigned to]\"]\n", node->ID,
-      //                varRef.toString());
-      //} else {
-      //  buffer.format("  N{} [label=\"{}\\n[Assigned to @({})]\"]\n", node->ID,
-      //                varRef.toString(),
-      //                toString(node->edgeKind));
-      //}
-      buffer.format("  N{} [label=\"VarRef {}\"]\n", node->ID, varRef.symbol.name);
+      // if (!varRef.isLeftOperand()) {
+      //   buffer.format("  N{} [label=\"{}\\n\"]\n", node->ID,
+      //   varRef.toString());
+      // } else if (node->edgeKind == EdgeKind::None) {
+      //   buffer.format("  N{} [label=\"{}\\n[Assigned to]\"]\n", node->ID,
+      //                 varRef.toString());
+      // } else {
+      //   buffer.format("  N{} [label=\"{}\\n[Assigned to @({})]\"]\n",
+      //   node->ID,
+      //                 varRef.toString(),
+      //                 toString(node->edgeKind));
+      // }
+      buffer.format("  N{} [label=\"VarRef {}\"]\n", node->ID,
+                    varRef.symbol.name);
       break;
     }
     case NodeKind::Assignment: {
@@ -102,28 +105,53 @@ void printDOT(const NetlistGraph &netlist, const std::string &fileName) {
 }
 
 int main(int argc, char **argv) {
+  OS::setupConsole();
+
   Driver driver;
   driver.addStandardArgs();
 
   std::optional<bool> showHelp;
   std::optional<bool> showVersion;
-  std::optional<std::string> netlistDotFile;
   driver.cmdLine.add("-h,--help", showHelp, "Display available options");
   driver.cmdLine.add("--version", showVersion,
                      "Display version information and exit");
 
+  std::optional<std::string> astJsonFile;
+  driver.cmdLine.add("--ast-json", astJsonFile,
+                     "Dump the compiled AST in JSON format to the specified "
+                     "file, or '-' for stdout",
+                     "<file>", CommandLineFlags::FilePath);
+
+  std::vector<std::string> astJsonScopes;
+  driver.cmdLine.add(
+      "--ast-json-scope", astJsonScopes,
+      "When dumping AST to JSON, include only the scopes specified by the "
+      "given hierarchical paths",
+      "<path>");
+
+  std::optional<std::string> netlistDotFile;
   driver.cmdLine.add(
       "--netlist-dot", netlistDotFile,
       "Dump the netlist in DOT format to the specified file, or '-' for stdout",
       "<file>", CommandLineFlags::FilePath);
+
+  std::optional<std::string> fromPointName;
+  driver.cmdLine.add("--from", fromPointName,
+                     "Specify a start point from which to trace a path",
+                     "<name>");
+
+  std::optional<std::string> toPointName;
+  driver.cmdLine.add("--to", toPointName,
+                     "Specify a finish point to trace a path to", "<name>");
 
   if (!driver.parseCommandLine(argc, argv)) {
     return 1;
   }
 
   if (showHelp == true) {
-    printf("%s\n",
-           driver.cmdLine.getHelpText("slang SystemVerilog compiler").c_str());
+    std::cout << fmt::format(
+        "{}\n",
+        driver.cmdLine.getHelpText("slang SystemVerilog netlist tool").c_str());
     return 0;
   }
 
@@ -134,8 +162,9 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  if (!driver.processOptions())
+  if (!driver.processOptions()) {
     return 2;
+  }
 
   bool ok = driver.parseAllSources();
   auto compilation = driver.createCompilation();
