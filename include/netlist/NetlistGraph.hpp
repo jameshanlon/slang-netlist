8c29af4d306430b6b56d6440a60602528a76e748
James Hanlon <mail@jameswhanlon.com>
2025-07-15 12:16:10 +0100

    Add plumbing for mergeDrivers

--- include/netlist/NetlistGraph.hpp
+++ include/netlist/NetlistGraph.hpp
@@ -12,16 +12,17 @@
 
 namespace slang::netlist {
 
+using SymbolSlotMap = std::map<const ast::ValueSymbol *, uint32_t>;
+using SymbolDriverMap = IntervalMap<uint64_t, NetlistNode *, 8>;
+
 /// Represent the netlist connectivity of an elaborated design.
 class NetlistGraph : public DirectedGraph<NetlistNode, NetlistEdge> {
-
-  using SymbolDriverMap = IntervalMap<uint64_t, NetlistNode *, 8>;
 
   BumpAllocator allocator;
   SymbolDriverMap::allocator_type mapAllocator;
 
   // Maps visited symbols to slots in driverMap vector.
-  std::map<const ast::ValueSymbol *, uint32_t> symbolToSlot;
+  SymbolSlotMap symbolToSlot;
 
   // For each symbol, map intervals to the netlist node driver.
   std::vector<SymbolDriverMap> driverMap;
@@ -44,92 +45,58 @@ public:
     return nullptr;
   }
 
-  /// @brief Called when an R value is encountered during netlist construction.
-  /// @param symbol The symbol for the R value.
-  /// @param bounds The range of the R value that is being used.
-  void handleRvalue(const ast::ValueSymbol &symbol,
-                    std::pair<uint32_t, uint32_t> bounds, NetlistNode *node) {
-    DEBUG_PRINT("Handle R-value: {} [{}:{}]\n", symbol.name, bounds.first,
-                bounds.second);
-
-    if (symbolToSlot.contains(&symbol)) {
-      // Symbol is assigned in this procedural block.
-
-      auto index = symbolToSlot.at(&symbol);
-      auto &definitions = driverMap[index];
-
-      // auto const *driver = getDriver(symbol, bounds);
-      // SLANG_ASSERT(driver);
-
-      for (auto it = definitions.find(bounds); it != definitions.end(); it++) {
-        auto itBounds = it.bounds();
-
-        // R-value bounds completely contains a definition bounds.
-        if (ConstantRange(itBounds).contains(ConstantRange(bounds))) {
-          // Add an edge from the definition node to the current node using it.
-          if (node) {
-            auto &edge = addEdge(**it, *node);
-            edge.setVariable(&symbol, bounds);
-          }
-        }
-
-        // R-value bounds completely contain a definition bounds.
-        if (ConstantRange(bounds).contains(ConstantRange(itBounds))) {
-          // Add an edge from the definition node to the current node using it.
-          SLANG_ASSERT(node);
-          auto &edge = addEdge(**it, *node);
-          edge.setVariable(&symbol, bounds);
-        }
-      }
-    } else {
-      // Otherwise, the symbol is unknown...
-      DEBUG_PRINT("No driver for {}\n", symbol.name);
-    }
+  /// @brief Merge symbol drivers from a procedural data flow analysis into the
+  /// driver map for the program.
+  /// @param symbolToSlot Mapping from symbols to slot indices.
+  /// @param procDriverMap Mapping from ranges to graph nodes.
+  auto mergeDrivers(SymbolSlotMap const &procSymbolToSlot,
+                    std::vector<SymbolDriverMap> const &procDriverMap) {
+    // TODO
   }
 
   /// @brief Called when an L value is encountered during netlist construction.
   /// @param symbol The L value symbol.
   /// @param lsp The expression containing the symbol and its selectors.
   /// @param bounds The range of the symbol that is being assigned to.
-  auto handleLvalue(const ast::ValueSymbol &symbol, const ast::Expression &lsp,
-                    std::pair<uint32_t, uint32_t> bounds, NetlistNode *node) {
+  auto handleLvalue(const ast::ValueSymbol &symbol,
+                    std::pair<uint32_t, uint32_t> bounds) {
     DEBUG_PRINT("Handle L-value: {} [{}:{}]\n", symbol.name, bounds.first,
                 bounds.second);
 
-    // Update visited symbols to slots.
-    auto [it, inserted] =
-        symbolToSlot.try_emplace(&symbol, (uint32_t)symbolToSlot.size());
+    //// Update visited symbols to slots.
+    // auto [it, inserted] =
+    //     symbolToSlot.try_emplace(&symbol, (uint32_t)symbolToSlot.size());
 
-    // Update current state definitions.
-    auto index = it->second;
-    if (index >= driverMap.size()) {
-      driverMap.emplace_back();
-    }
+    //// Update current state definitions.
+    // auto index = it->second;
+    // if (index >= driverMap.size()) {
+    //   driverMap.emplace_back();
+    // }
 
-    auto &definitions = driverMap[index];
-    for (auto it = definitions.find(bounds); it != definitions.end();) {
+    // auto &definitions = driverMap[index];
+    // for (auto it = definitions.find(bounds); it != definitions.end();) {
 
-      auto itBounds = it.bounds();
+    //  auto itBounds = it.bounds();
 
-      // Existing entry completely contains new bounds, so split that entry.
-      if (ConstantRange(itBounds).contains(ConstantRange(bounds))) {
-        definitions.erase(it, mapAllocator);
-        definitions.insert({itBounds.first, bounds.first}, *it, mapAllocator);
-        definitions.insert({bounds.second, itBounds.second}, *it, mapAllocator);
-        break;
-      }
+    //  // Existing entry completely contains new bounds, so split that entry.
+    //  if (ConstantRange(itBounds).contains(ConstantRange(bounds))) {
+    //    definitions.erase(it, mapAllocator);
+    //    definitions.insert({itBounds.first, bounds.first}, *it, mapAllocator);
+    //    definitions.insert({bounds.second, itBounds.second}, *it,
+    //    mapAllocator); break;
+    //  }
 
-      // New bounds completely contain an existing entry, so delete that entry.
-      if (ConstantRange(bounds).contains(ConstantRange(itBounds))) {
-        definitions.erase(it, mapAllocator);
-        it = definitions.find(bounds); // FIXME?
-      } else {
-        ++it;
-      }
-    }
+    //  // New bounds completely contain an existing entry, so delete that
+    //  entry. if (ConstantRange(bounds).contains(ConstantRange(itBounds))) {
+    //    definitions.erase(it, mapAllocator);
+    //    it = definitions.find(bounds); // FIXME?
+    //  } else {
+    //    ++it;
+    //  }
+    //}
 
-    // Insert the new definition into the DFA state.
-    definitions.insert(bounds, node, mapAllocator);
+    //// Insert the new definition into the DFA state.
+    // definitions.insert(bounds, node, mapAllocator);
   }
 };
 
