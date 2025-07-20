bb72f6596976c93fab57d501266885d0a468016a
James Hanlon <mail@jameswhanlon.com>
2025-07-20 21:54:34 +0100

    Hook up JSON printing

--- tools/driver/driver.cpp
+++ tools/driver/driver.cpp
@@ -1,6 +1,7 @@
 #include "slang/driver/Driver.h"
 #include "slang/ast/Compilation.h"
 #include "slang/text/FormatBuffer.h"
+#include "slang/text/Json.h"
 #include "slang/util/VersionInfo.h"
 
 #include "netlist/NetlistDot.hpp"
@@ -11,6 +12,24 @@ using namespace slang;
 using namespace slang::ast;
 using namespace slang::driver;
 using namespace slang::netlist;
+
+void printJson(Compilation &compilation, const std::string &fileName,
+               const std::vector<std::string> &scopes) {
+  JsonWriter writer;
+  writer.setPrettyPrint(true);
+  ASTSerializer serializer(compilation, writer);
+  if (scopes.empty()) {
+    serializer.serialize(compilation.getRoot());
+  } else {
+    for (auto &scopeName : scopes) {
+      auto sym = compilation.getRoot().lookupName(scopeName);
+      if (sym) {
+        serializer.serialize(*sym);
+      }
+    }
+  }
+  OS::writeFile(fileName, writer.view());
+}
 
 int main(int argc, char **argv) {
   OS::setupConsole();
@@ -78,32 +97,56 @@ int main(int argc, char **argv) {
     return 2;
   }
 
-  if (debug.value_or(false)) {
+  if (debug) {
     Config::getInstance().debugEnabled = true;
   }
 
-  if (quiet.value_or(false)) {
+  if (quiet) {
     Config::getInstance().quietEnabled = true;
   }
 
-  bool ok = driver.parseAllSources();
-  auto compilation = driver.createCompilation();
-  driver.reportCompilation(*compilation, true);
-  auto analysisManager = driver.runAnalysis(*compilation);
-  ok |= driver.reportDiagnostics(true);
+  SLANG_TRY {
 
-  NetlistGraph graph;
-  NetlistVisitor visitor(*compilation, *analysisManager, graph);
-  compilation->getRoot().visit(visitor);
-  graph.processPendingRvalues();
+    bool ok = driver.parseAllSources();
+    auto compilation = driver.createCompilation();
+    driver.reportCompilation(*compilation, true);
+    ok |= driver.reportDiagnostics(true);
 
-  // Output a DOT file of the netlist.
-  if (netlistDotFile) {
-    FormatBuffer buffer;
-    NetlistDot::render(graph, buffer);
-    OS::writeFile(*netlistDotFile, buffer.str());
-    return 0;
+    if (astJsonFile) {
+      printJson(*compilation, *astJsonFile, astJsonScopes);
+      return 0;
+    }
+
+    auto analysisManager = driver.runAnalysis(*compilation);
+    ok |= driver.reportDiagnostics(true);
+
+    if (!ok) {
+      return ok;
+    }
+
+    NetlistGraph graph;
+    NetlistVisitor visitor(*compilation, *analysisManager, graph);
+    compilation->getRoot().visit(visitor);
+    graph.processPendingRvalues();
+
+    DEBUG_PRINT("Netlist has {} nodes and {} edges\n", graph.numNodes(),
+                graph.numEdges());
+
+    // Output a DOT file of the netlist.
+    if (netlistDotFile) {
+      FormatBuffer buffer;
+      NetlistDot::render(graph, buffer);
+      OS::writeFile(*netlistDotFile, buffer.str());
+      return 0;
+    }
+
+    // No action performed.
+    return 1;
+  }
+  SLANG_CATCH(const std::exception &e) {
+    SLANG_REPORT_EXCEPTION(e, "{}\n");
+    return 1;
   }
 
-  return ok ? 0 : 3;
+  return 0;
 }
