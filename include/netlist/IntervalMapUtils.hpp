c6dfe6353fdcc8d4ee81252674ed2c0c748de0c9
James Hanlon <mail@jameswhanlon.com>
2025-08-01 21:58:27 +0100

    Fix difference

--- include/netlist/IntervalMapUtils.hpp
+++ include/netlist/IntervalMapUtils.hpp
@@ -7,6 +7,49 @@ namespace slang::netlist {
 
 struct IntervalMapUtils {
 
+  template <typename TKey, typename TValue, uint32_t N>
+  static void
+  subtractSingle(IntervalMap<TKey, TValue, N> &result,
+                 std::pair<TKey, TKey> const &interval, TValue const &value,
+                 IntervalMap<TKey, TValue, N> const &second,
+                 IntervalMap<TKey, TValue, N>::allocator_type &alloc) {
+    TKey start = interval.first;
+    TKey end = interval.second;
+    TKey current = start;
+
+    for (auto it = second.begin(); it != second.end(); ++it) {
+      auto rbounds = it.bounds();
+
+      if (rbounds.second < current) {
+        // Right interval is before the current interval.
+        continue;
+      }
+
+      if (rbounds.first > end) {
+        // Right interval is after the current interval.
+        continue;
+      }
+
+      if (rbounds.first >= current) {
+        // Right interval overlaps with the current interval.
+        result.unionWith(current, std::min(end, rbounds.first - 1), value,
+                         alloc);
+      }
+
+      // Move current to the end of the right interval.
+      current = std::max(current, rbounds.second + 1);
+
+      if (current >= end) {
+        // If current has reached or exceeded the end of the interval, break.
+        break;
+      }
+    }
+
+    if (current <= end) {
+      result.unionWith(current, end, value, alloc);
+    }
+  }
+
   /// Construct the difference between two IntervalMaps.
   template <typename TKey, typename TValue, uint32_t N>
   static IntervalMap<TKey, TValue, N>
@@ -14,28 +57,15 @@ struct IntervalMapUtils {
              IntervalMap<TKey, TValue, N> const &second,
              IntervalMap<TKey, TValue, N>::allocator_type &alloc) {
 
+    if (second.empty()) {
+      // If the second map is empty, return the first map.
+      return first.clone(alloc);
+    }
+
     IntervalMap<TKey, TValue, N> result;
 
-    auto lit = first.begin();
-    auto rit = second.begin();
-    auto lend = first.end();
-    auto rend = second.end();
-
-    while (lit != lend && rit != rend) {
-      auto lkey = lit.bounds();
-      auto rkey = rit.bounds();
-      if (lkey.second < rkey.first) {
-        result.unionWith(lkey.first, lkey.second, *lit, alloc);
-        ++lit;
-      } else if (rkey.second < lkey.first) {
-        ++rit;
-      } else if (lkey.second < rkey.second) {
-        result.unionWith(lkey.first, rkey.first, *lit, alloc);
-        ++lit;
-      } else {
-        result.unionWith(rkey.second, lkey.second, *lit, alloc);
-        ++rit;
-      }
+    for (auto it = first.begin(); it != first.end(); ++it) {
+      subtractSingle(result, it.bounds(), *it, second, alloc);
     }
 
     return result;
