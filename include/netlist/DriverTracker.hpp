eb095e24b137a05b4c42be632cd5e8a290ba4935
James Hanlon <mail@jameswhanlon.com>
2025-09-30 21:30:48 +0100

    Add DriverTracker

---
+++ include/netlist/DriverTracker.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "netlist/NetlistNode.hpp"
+
+#include "slang/ast/Expression.h"
+#include "slang/ast/Symbol.h"
+#include "slang/ast/symbols/ValueSymbol.h"
+#include "slang/text/FormatBuffer.h"
+#include "slang/util/IntervalMap.h"
+#include "slang/util/SmallVector.h"
+
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+namespace slang::netlist {
+
+/// Information about a driver of a particular range of a symbol.
+struct DriverInfo {
+  NetlistNode *node;
+  const ast::Expression *lsp;
+};
+
+/// A range over which a symbol is driven.
+using DriverBitRange = std::pair<uint32_t, uint32_t>;
+
+/// A list of drivers for a particular range of a symbol.
+using DriverList = SmallVector<DriverInfo, 4>;
+
+/// A map of driven ranges of a symbol.
+using SymbolDriverMap = IntervalMap<uint32_t, DriverList, 8>;
+
+/// Map symbols to indexes.
+using SymbolSlotMap = std::map<const ast::Symbol *, uint32_t>;
+
+/// Map indexes to symbols.
+using SlotSymbolMap = std::vector<const ast::Symbol *>;
+
+/// Per-symbol SymbolDriverMaps.
+using SymbolDrivers = std::vector<SymbolDriverMap>;
+
+/// Track drivers for symbols.
+///
+/// Each symbol encountered in the AST has an interval map where each interval
+/// is a range that is driven by one or more statements in the design. Intervals
+/// are non-overlapping, each interval maps to a list of DriverInfo objects, and
+/// adjacent intervals have different driver lists.
+///
+/// Note that SymbolDrivers is not a member of this class because it is stored
+/// in the analysis state during the DataFlowAnalysis pass.
+class DriverTracker {
+
+  BumpAllocator allocator;
+  SymbolDriverMap::allocator_type mapAllocator;
+
+  // Map symbols to indexes in vectors of SymbolDriverMaps.
+  SymbolSlotMap symbolToSlot;
+
+  // The reverse mapping of slot indexes to symbols.
+  SlotSymbolMap slotToSymbol;
+
+public:
+  DriverTracker() : mapAllocator(allocator) {}
+
+  /// Add a driver for the specified symbol. This overwrites any existing
+  /// drivers for the specified bit range.
+  void addDriver(SymbolDrivers &drivers, ast::ValueSymbol const &symbol,
+                 ast::Expression const &lsp, DriverBitRange bounds,
+                 NetlistNode *node);
+
+  /// Merge a driver for the specified symbol. This adds to any existing
+  /// drivers for the specified bit range.
+  void mergeDriver(SymbolDrivers &drivers, ast::ValueSymbol const &symbol,
+                   ast::Expression const &lsp, DriverBitRange bounds,
+                   NetlistNode *node);
+
+  /// Return a list of all the drivers for the given symbol and bit range.
+  /// If there are no drivers, the returned list will be empty.
+  auto getDrivers(SymbolDrivers &drivers, ast::Symbol const &symbol,
+                  DriverBitRange bounds) -> DriverList;
+
+  /// Dump the current driver map for all symbols for debugging output.
+  auto dumpDrivers(ast::Symbol const &symbol, SymbolDriverMap &driverMap)
+      -> std::string;
+};
+
+} // namespace slang::netlist
---
+++ source/DriverTracker.cpp
@@ -0,0 +1,162 @@
+#include "netlist/DriverTracker.hpp"
+#include "netlist/Debug.hpp"
+
+using namespace slang::netlist;
+
+void DriverTracker::addDriver(SymbolDrivers &drivers,
+                              ast::ValueSymbol const &symbol,
+                              ast::Expression const &lsp, DriverBitRange bounds,
+                              NetlistNode *node) {
+
+  // Update visited symbols to slots.
+  auto [it, inserted] =
+      symbolToSlot.try_emplace(&symbol, (uint32_t)symbolToSlot.size());
+  auto index = it->second;
+
+  // Resize drivers vector if necessary.
+  if (index >= drivers.size()) {
+    drivers.resize(index + 1);
+  }
+
+  // Resize slotToSymbol vector if necessary.
+  if (index >= slotToSymbol.size()) {
+    slotToSymbol.resize(index + 1);
+    slotToSymbol[index] = &symbol;
+  }
+
+  DEBUG_PRINT("Add driver [{}:{}] for symbol={} index={}: \n", bounds.first,
+              bounds.second, symbol.name, index);
+
+  auto &driverMap = drivers[index];
+
+  for (auto it = driverMap.find(bounds); it != driverMap.end();) {
+    DEBUG_PRINT("Examining existing definition: [{}:{}]\n", it.bounds().first,
+                it.bounds().second);
+
+    auto itBounds = it.bounds();
+    auto drivers = *it;
+
+    // An existing entry completely contains the new bounds, so split the
+    // existing entry to create an interval for the new driver.
+    //  Existing entry:    [---------------]
+    //  New bounds:           [-------]
+    if (ConstantRange(itBounds).contains(ConstantRange(bounds))) {
+      driverMap.erase(it, mapAllocator);
+
+      // Left part.
+      if (itBounds.first < bounds.first) {
+        driverMap.insert({itBounds.first, bounds.first - 1}, drivers,
+                         mapAllocator);
+        DEBUG_PRINT("Split left [{}:{}]\n", itBounds.first, bounds.first - 1);
+      }
+
+      // Right part.
+      if (itBounds.second > bounds.second) {
+        driverMap.insert({bounds.second + 1, itBounds.second}, drivers,
+                         mapAllocator);
+        DEBUG_PRINT("Split right [{}:{}]\n", bounds.second + 1,
+                    itBounds.second);
+      }
+
+      // Middle part (with new driver).
+      driverMap.insert(bounds, {DriverInfo(node, &lsp)}, mapAllocator);
+      DEBUG_PRINT("Inserting new definition: [{}:{}]\n", bounds.first,
+                  bounds.second);
+
+      // No more intervals to compare against.
+      return;
+    }
+
+    // The new bounds completely contains an existing entry, so delete that
+    // entry.
+    //   Existing entry:    [-------]
+    //   New bounds:     [---------------]
+    if (ConstantRange(bounds).contains(ConstantRange(itBounds))) {
+      driverMap.erase(it, mapAllocator);
+      it = driverMap.find(bounds);
+      DEBUG_PRINT("Erased existing definition\n");
+      continue;
+    }
+
+    // Existing entry left-overlaps new bounds.
+    //   Existing entry:  [-------]
+    //   New bounds:            [-------]
+    if (itBounds.first <= bounds.first && itBounds.second >= bounds.first) {
+      driverMap.erase(it, mapAllocator);
+
+      // Left part.
+      driverMap.insert({itBounds.first, bounds.first - 1}, drivers,
+                       mapAllocator);
+      DEBUG_PRINT("Split left [{}:{}]\n", itBounds.first, bounds.first - 1);
+
+      // Overlapping part (with new driver).
+      driverMap.insert({bounds.first, itBounds.second},
+                       {DriverInfo(node, &lsp)}, mapAllocator);
+      DEBUG_PRINT("Inserting new definition: [{}:{}]\n", bounds.first,
+                  itBounds.second);
+
+      // Adjust the bounds to continue searching for overlaps.
+      bounds.first = itBounds.second + 1;
+      it = driverMap.find(bounds);
+      continue;
+    }
+
+    // Existing entry right-overlaps new bounds.
+    //   Existing entry:          [-------]
+    //   New bounds:        [-------]
+    if (itBounds.first <= bounds.second && itBounds.second >= bounds.second) {
+      driverMap.erase(it, mapAllocator);
+
+      // Right part of existing entry.
+      driverMap.insert({bounds.second + 1, itBounds.second}, drivers,
+                       mapAllocator);
+      DEBUG_PRINT("Split right [{}:{}]\n", bounds.second + 1, itBounds.second);
+
+      // Left part (with new driver).
+      driverMap.insert({bounds.first, bounds.second}, {DriverInfo(node, &lsp)},
+                       mapAllocator);
+      DEBUG_PRINT("Inserting new definition: [{}:{}]\n", bounds.first,
+                  bounds.second);
+
+      // No more overlaps possible.
+      return;
+    }
+
+    // Skip interval.
+    ++it;
+  }
+}
+
+void DriverTracker::mergeDriver(SymbolDrivers &drivers,
+                                ast::ValueSymbol const &symbol,
+                                ast::Expression const &lsp,
+                                DriverBitRange bounds, NetlistNode *node) {
+  // TODO
+}
+
+auto DriverTracker::getDrivers(SymbolDrivers &drivers,
+                               ast::Symbol const &symbol, DriverBitRange bounds)
+    -> DriverList {
+  DriverList result;
+  if (symbolToSlot.contains(&symbol)) {
+    SLANG_ASSERT(drivers.size() > symbolToSlot[&symbol]);
+    auto &map = drivers[symbolToSlot[&symbol]];
+    for (auto it = map.find(bounds); it != map.end(); it++) {
+      if (ConstantRange(bounds).contains(ConstantRange(it.bounds()))) {
+        result.append((*it).begin(), (*it).end());
+      }
+    }
+  }
+  return result;
+}
+
+auto DriverTracker::dumpDrivers(ast::Symbol const &symbol,
+                                SymbolDriverMap &driverMap) -> std::string {
+  FormatBuffer out;
+  out.format("Driver map for symbol {}:\n", symbol.name);
+  for (auto it = driverMap.begin(); it != driverMap.end(); it++) {
+    out.format("[{}:{}] {} drivers\n", it.bounds().first, it.bounds().second,
+               (*it).size());
+  }
+  return out.str();
+}
