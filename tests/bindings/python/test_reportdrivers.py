9853ba6ea2537ef26011d948dd418133bedd72d0
James Hanlon <mail@jameswhanlon.com>
2025-10-08 22:49:25 +0100

    Add Python bindings for ReportDrivers (#26)
    * Add Python bindings for ReportDrivers
    
    * Set python environment
    
    * Setup slang to build pyslang
    
    * Remove binary files
    
    * Make pyslang import work
    
    * WIP construction test
    
    * Hook up visitor
    
    * Add assert
    
    * Add file
    
    * Disable python bindings for sanitizer tests
    
    * Tidy

---
+++ tests/bindings/python/test_reportdrivers.py
@@ -0,0 +1,37 @@
+import unittest
+
+import pyslang
+import pyslang_netlist
+
+
+class TestReportDrivers(unittest.TestCase):
+    def test_reportdrivers_import(self):
+        self.assertTrue(hasattr(pyslang_netlist, "ReportDrivers"))
+
+    def test_reportdrivers_methods(self):
+        cls = pyslang_netlist.ReportDrivers
+        self.assertTrue(hasattr(cls, "report"))
+
+    def test_reportdrivers(self):
+        code = "module m(output logic a); assign a = 1; endmodule"
+
+        # Compile the test.
+        tree = pyslang.SyntaxTree.fromText(code)
+        compilation = pyslang.Compilation()
+        compilation.addSyntaxTree(tree)
+        diagnostics = compilation.getAllDiagnostics()
+        assert len(diagnostics) == 0
+        # compilation.freeze()
+
+        # Run analysis.
+        analysis_manager = pyslang.AnalysisManager()
+        # analysis_manager.analyze(compilation)
+
+        # Report drivers.
+        report_drivers = pyslang_netlist.ReportDrivers(compilation, analysis_manager)
+        report_drivers.run(compilation)
+        self.assertTrue("m.a" in report_drivers.report())
+
+
+if __name__ == "__main__":
+    unittest.main()
