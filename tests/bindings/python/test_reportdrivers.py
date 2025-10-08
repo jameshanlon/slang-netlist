e9fcc484532231613a229a829909bc5f9d227fc1
James Hanlon <mail@jameswhanlon.com>
2025-10-08 08:34:25 +0100

    Set python environment

--- tests/bindings/python/test_reportdrivers.py
+++ tests/bindings/python/test_reportdrivers.py
@@ -4,13 +4,17 @@ import py_slang_netlist
 
 
 class TestReportDrivers(unittest.TestCase):
-    def test_reportdrivers_import(self):
-        # Just test that the class can be imported and constructed
+
+    def test_import(self):
+        """
+        Test that the class can be imported and constructed.
+        """
         self.assertTrue(hasattr(py_slang_netlist, "ReportDrivers"))
 
-    def test_reportdrivers_methods(self):
-        # We cannot construct ReportDrivers without C++ objects, but we can
-        # check method presence
+    def test_methods_exist(self):
+        """
+        Test that the class methods exits.
+        """
         cls = py_slang_netlist.ReportDrivers
         self.assertTrue(hasattr(cls, "report"))
 
