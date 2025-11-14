f590a537f4e331a414a517044515461f77f11ddd
James Hanlon <mail@jameswhanlon.com>
2025-11-14 14:50:10 +0000

    Documentation updates

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,13 @@
 DOXYFILE = "Doxyfile"
 
-# LINKS_NAVBAR1 = [
-#    (None, 'pages', [(None, 'about')]),
-#    (None, 'namespaces', []),
-# `]
+LINKS_NAVBAR1 = [
+    (None, "getting-started", []),
+    (None, "developer", []),
+]
+
+LINKS_NAVBAR2 = [
+    (None, "namespaces", []),
+    (None, "annotated", []),
+    (None, "files", []),
+    ('<a href="https://github.com/jameshanlon/slang-netlist">GitHub</a>', []),
+]
