82aff452f2b618e5726922c461826267fd8a7db2
James Hanlon <mail@jameswhanlon.com>
2025-12-22 12:54:59 +0000

    One more test case

--- tests/unit/DriverTests.cpp
+++ tests/unit/DriverTests.cpp
@@ -1,6 +1,6 @@
 #include "Test.hpp"
 
-TEST_CASE("New driver range that contains an existing one") {
+TEST_CASE("Driver range that contains an existing one") {
   auto const &tree = (R"(
 module m(input logic [3:0] a, output logic [3:0] b);
   logic [3:0] t;
@@ -16,7 +16,7 @@ endmodule
   CHECK(test.getDrivers("m.t", {1, 0}).size() == 1);
 }
 
-TEST_CASE("New driver range that left-overlaps an existing one (replace)") {
+TEST_CASE("Driver range that left-overlaps an existing one (replace)") {
   auto const &tree = (R"(
 module m(input logic [3:0] a, output logic [3:0] b);
   logic [3:0] t;
@@ -33,7 +33,7 @@ endmodule
   CHECK(test.getDrivers("m.t", {1, 0}).size() == 1);
 }
 
-TEST_CASE("New driver range that right-overlaps an existing one (replace)") {
+TEST_CASE("Driver range that right-overlaps an existing one (replace)") {
   auto const &tree = (R"(
 module m(input logic [3:0] a, output logic [3:0] b);
   logic [3:0] t;
@@ -50,7 +50,7 @@ endmodule
   CHECK(test.getDrivers("m.t", {1, 0}).size() == 1);
 }
 
-TEST_CASE("New driver range that left-overlaps an existing one (merge)") {
+TEST_CASE("Driver range that left-overlaps an existing one (merge)") {
   auto const &tree = (R"(
 module m(input logic [3:0] a, input logic c, output logic [3:0] b);
   logic [3:0] t;
@@ -69,7 +69,7 @@ endmodule
   CHECK(test.getDrivers("m.t", {1, 0}).size() == 1);
 }
 
-TEST_CASE("New driver range that right-overlaps an existing one (merge)") {
+TEST_CASE("Driver range that right-overlaps an existing one (merge)") {
   auto const &tree = (R"(
 module m(input logic [3:0] a, input logic c, output logic [3:0] b);
   logic [3:0] t;
@@ -86,4 +86,26 @@ endmodule
   CHECK(test.getDrivers("m.t", {3, 3}).size() == 1);
   CHECK(test.getDrivers("m.t", {2, 2}).size() == 2);
   CHECK(test.getDrivers("m.t", {1, 0}).size() == 1);
+}
+
+TEST_CASE("Four-way driver overlap (merge)") {
+  auto const &tree = (R"(
+module m(input logic [3:0] a, input logic [1:0] c, output logic [3:0] b);
+  logic [3:0] t;
+  always_comb begin
+    case (c)
+    0: t[1:0] = a[1:0];
+    1: t[3:2] = a[1:0];
+    2: t[2:1] = a[1:0];
+    3: t[1] = a[0];
+    endcase
+  end
+  assign b = t;
+endmodule
+)");
+  NetlistTest test(tree);
+  CHECK(test.getDrivers("m.t", {3, 3}).size() == 1);
+  CHECK(test.getDrivers("m.t", {2, 2}).size() == 2);
+  CHECK(test.getDrivers("m.t", {1, 1}).size() == 3);
+  CHECK(test.getDrivers("m.t", {0, 0}).size() == 1);
 }
