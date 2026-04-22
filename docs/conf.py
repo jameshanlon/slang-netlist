d712e70412307b8c020521379c827c2f843b550d
James Hanlon <mail@jameswhanlon.com>
2026-04-22 10:20:11 +0100

    Reorganise user guide

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,22 @@
 DOXYFILE = "Doxyfile"
 
 LINKS_NAVBAR1 = [
-    (None, "user-guide", []),
+    (
+        "User guide",
+        "user-guide",
+        [
+            ('<a href="user-guide.html#getting-started">Getting started</a>',),
+            ('<a href="user-guide.html#cli-usage">CLI usage</a>',),
+            ('<a href="user-guide.html#python-bindings">Python bindings</a>',),
+            ('<a href="user-guide.html#performance">Performance tuning</a>',),
+            (
+                '<a href="user-guide.html#supported-constructs">'
+                "Supported constructs</a>",
+            ),
+            ('<a href="user-guide.html#analysis-model">Analysis model</a>',),
+            ('<a href="user-guide.html#common-workflows">Common workflows</a>',),
+        ],
+    ),
     (None, "developer-guide", []),
 ]
 
