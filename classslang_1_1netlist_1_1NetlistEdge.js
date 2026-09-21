7a7b63ae805b57c3878265141f4be0e35114967d
jameshanlon <jameshanlon@users.noreply.github.com>
2026-09-21 10:44:19 +0000

    deploy: d7af177190f8808c29bff4644c5a492dda64f7ac

--- classslang_1_1netlist_1_1NetlistEdge.js
+++ classslang_1_1netlist_1_1NetlistEdge.js
@@ -4,7 +4,7 @@ var classslang_1_1netlist_1_1NetlistEdge =
     [ "disable", "classslang_1_1netlist_1_1NetlistEdge.html#ad1f1e466614af05abbd724bb1621510d", null ],
     [ "hasSymbol", "classslang_1_1netlist_1_1NetlistEdge.html#aa725c80361be5b8a7609549732177ee9", null ],
     [ "setEdgeKind", "classslang_1_1netlist_1_1NetlistEdge.html#a2dc15551fdea30c1a1bbd4ea58e8e7c0", null ],
-    [ "setVariable", "classslang_1_1netlist_1_1NetlistEdge.html#a9b596d3128d14fa7302e8abbcba4fe3c", null ],
+    [ "setVariable", "classslang_1_1netlist_1_1NetlistEdge.html#aefe7dc1f1d30d19bef78926466bb8ca9", null ],
     [ "bounds", "classslang_1_1netlist_1_1NetlistEdge.html#a3841bbbdef07d6ff7a3a4ac81e6bd35f", null ],
     [ "disabled", "classslang_1_1netlist_1_1NetlistEdge.html#a50d40cc0578699cf304b585e78c1b88a", null ],
     [ "edgeKind", "classslang_1_1netlist_1_1NetlistEdge.html#ad9caa28143c7f52cc087ecb04d40d05f", null ],
--- search/all_11.js
+++ search/all_11.js
@@ -6,7 +6,7 @@ var searchData=
   ['setbuildprofile_3',['setBuildProfile',['../classslang_1_1netlist_1_1NetlistGraph.html#a1054466ed0f74ec8eeff8f3a29b6e923',1,'slang::netlist::NetlistGraph']]],
   ['setedgekind_4',['setEdgeKind',['../classslang_1_1netlist_1_1NetlistEdge.html#a2dc15551fdea30c1a1bbd4ea58e8e7c0',1,'slang::netlist::NetlistEdge']]],
   ['setnamefilters_5',['setNameFilters',['../classslang_1_1report_1_1ReportVisitorBase.html#a8d6af2ad3972785e820b5fac81d9ff57',1,'slang::report::ReportVisitorBase']]],
-  ['setvariable_6',['setVariable',['../classslang_1_1netlist_1_1NetlistEdge.html#a9b596d3128d14fa7302e8abbcba4fe3c',1,'slang::netlist::NetlistEdge']]],
+  ['setvariable_6',['setVariable',['../classslang_1_1netlist_1_1NetlistEdge.html#aefe7dc1f1d30d19bef78926466bb8ca9',1,'slang::netlist::NetlistEdge']]],
   ['size_7',['size',['../classslang_1_1netlist_1_1FormatBuffer.html#a59a1713e4c9f8899fa0e798ece065024',1,'slang::netlist::FormatBuffer::size()'],['../classslang_1_1netlist_1_1NetlistPath.html#a4c37d125fdc7f9fb02725840f64be86a',1,'slang::netlist::NetlistPath::size()'],['../classslang_1_1netlist_1_1SymbolTable.html#ab1b69b12f08e36a60b454cd6e221c4ac',1,'slang::netlist::SymbolTable::size()'],['../classslang_1_1netlist_1_1FileTable.html#a5b6a6cabcf14d9946be5375565ec00fa',1,'slang::netlist::FileTable::size()']]],
   ['slang_8',['slang',['../namespaceslang.html',1,'']]],
   ['slang_20netlist_9',['slang netlist',['../user-guide.html#cli-general',1,'General options (slang-netlist)'],['../index.html',1,'slang-netlist']]],
